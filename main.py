1e3f28ee53afcf766f14077274f03d112500c5b5
qq_36675042 <qq_36675042@devide>
2025-04-16 15:58:00 +0800

    Wed Apr 16 15:58:00 CST 2025 inscode

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print('欢迎来到 InsCode')
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.schema import ChatMessage
+from langchain_openai import ChatOpenAI
+import streamlit as st
+from dotenv import load_dotenv
+importrt os
+
+load_dotenv()
+OPENAI_API_KEY = os.envirronment.get("OPENAI_API_KEY")
+
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
+
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", type="password")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [ChatMessage(role="assistant", content="How can I help you?")]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg.role).write(msg.content)
+
+if prompt := st.chat_input():
+    st.session_state.messages.append(ChatMessage(role="user", content=prompt))
+    st.chat_message("user").write(prompt)
+
+    if not openai_api_key:
+        st.info("请输入 OpenAI API key.")
+        st.stop()
+
+    with st.chat_message("assistant"):
+        stream_handler = StreamHandler(st.empty())
+        llm = ChatOpenAI(
+            openai_api_key=openai_api_key, 
+            model_name="Qwen/QwQ-32B", 
+            streaming=True, 
+            callbacks=[stream_handler]
+        )
+        response = llm.invoke(st.session_state.messages)
+        st.session_state.messages.append(ChatMessage(role="assistant", content=response.content))
